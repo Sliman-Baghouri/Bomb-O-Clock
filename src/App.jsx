13a7c2f75384209a4bb5c239cd4b9e8543e7ae04
Jovane Castro <jovanecastro12@gmail.com>
2022-05-20 14:22:04 -0300

    :sparkles: feat: Title
    'Bomb O'Clock' is written on header

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react';
 import './App.css';
 import Bomb from './components/Bomb';
@@ -7,6 +8,11 @@ import Footer from './components/Footer';
 export default function App() {
   return (
     <>
+      <header className="header">
+        <h1>
+          🧨 Bomb O'Clock ⏱️
+        </h1>
+      </header>
       <main className="App">
         <Bomb />
         <BombButtons />
